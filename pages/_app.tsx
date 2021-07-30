ebcca11537950ce7b54689eb6366df4d2246a6a9
Edgar Ehrenberg <ainshin@its.jnj.com>
2021-07-30 13:28:35 +0300

    Updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
+import React from "react";
 import { AppProps } from "next/dist/next-server/lib/router/router";
-import "../styles/globals.css";
+
+import Head from "next/head";
+
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Rating</title>
+        <meta name="description" content="Next app" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
